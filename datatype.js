4eded3a7ebf0f295b6a7e0097a054a16c3e4e9f4
palakdoremon <palakjn567@gmail.com>
2025-02-02 14:13:01 +0530

    datatypes

--- datatype.js
+++ datatype.js
@@ -4,9 +4,23 @@
 
 console.log(3+3)
 
+
+// number= 2to power of 53
+// BigInt
+// boolean=true/false
+// string = " " // double cords
+// null= standalone value means shows empty
+// undefined= not assigned any value
+
+
 let name="palak"
 let age=19
 let isLoggedIn= false
+let state=null;
+let state1;
 
 
-console.log(typeof age)
+console.log(typeof age)   // datatype is number
+console.log(typeof null);   // datatype is object
+console.log(typeof undefined);  // datatype is undefined
+
