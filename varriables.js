410d5b675f15a5819cc3555980d9317db95b8be7
palakdoremon <palakjn567@gmail.com>
2025-02-02 14:14:03 +0530

    varriables and constants

---
+++ varriables.js
@@ -0,0 +1,19 @@
+const accountId=1535717
+let accountEmail="palakjn56@gmail.com"
+var accountPassword="1535717"
+accountCity="Jaipur"
+let accountState;
+
+// accountId=2
+
+accountEmail="pj@gmail.com"
+accountPassword="1536"
+accountCity="goa"
+
+console.table([accountId,accountEmail,accountPassword,accountCity.accountState])//it is use to display output in table fotm
+
+// console.log(accountId);
+
+/* 
+   Prefer not to use var beacause of issue in block scope and functional scope
+*/
