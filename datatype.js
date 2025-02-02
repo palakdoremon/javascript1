b3fdb2eafa84657e118419e0f25ec218bf409584
palakdoremon <palakjn567@gmail.com>
2025-02-02 14:11:27 +0530

    datatypes

---
+++ datatype.js
@@ -0,0 +1,12 @@
+"use strict";        //treat all JS code as newer version
+
+// alert("hello") // we are using node js not browser
+
+console.log(3+3)
+
+let name="palak"
+let age=19
+let isLoggedIn= false
+
+
+console.log(typeof age)
