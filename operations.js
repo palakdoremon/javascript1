79e53a7db9b92fa9f07adfcecf01dc8f842ca204
palakdoremon <palakjn567@gmail.com>
2025-02-06 16:39:29 +0530

    operations

---
+++ operations.js
@@ -0,0 +1,51 @@
+let value=3
+let negvalue=-value
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1="hello"
+let str2=" palak"
+let str3=str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);   //output 12
+
+// console.log(1 +"2");   //output 12
+
+// console.log("1" + "2");  //output 12
+
+// console.log("1" + 2 + 2); //output  122
+
+// console.log(1 + 2 + "2");  //outpot 32
+
+
+
+console.log(true)
+
+
+let num1 ,num2 ,num3
+
+num1=num2=num3=2+2
+
+console.log(num3);
+console.log(num2);
+console.log(num1);
+
+
+let gamecounter =100
+gamecounter++;
+console.log(gamecounter);
+
+
+
+
+
+
+
