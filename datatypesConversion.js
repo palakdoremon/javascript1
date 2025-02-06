40ac3f6bbf8af40c01ed7064b4ebb438e07afa7d
palakdoremon <palakjn567@gmail.com>
2025-02-06 16:46:44 +0530

    convert string in integer or vice versa

---
+++ datatypesConversion.js
@@ -0,0 +1,27 @@
+let score="33abc"
+// console.log(typeof 33)
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueNumber=Number(score)   // convert the string in number
+console.log(typeof valueNumber)
+console.log(valueNumber)
+
+//"33" = 33
+//"33abc"= NaN
+//true =1; false=0;
+
+
+// let isLoggedIn=1 // output true
+
+// let isLoggedIn=""  //output false
+
+let isLoggedIn="palak" //output true
+
+
+// console.log(typeof isLoggedIn);
+
+let BooleanisLoggedIn=Boolean(isLoggedIn)
+console.log(BooleanisLoggedIn);
+
+
