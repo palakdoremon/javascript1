c729f9988e723e85f14522f4ad0e39d51bd9aa92
palakdoremon <palakjn567@gmail.com>
2025-02-06 16:38:06 +0530

    comparision

---
+++ comparision.js
@@ -0,0 +1,29 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);   //datatype should be same but java by default change the striung in the number so output is true
+// console.log("2"<1);  
+
+
+// in tarike ke conversion ko avoid hi krna cahiye ye confuse krta h 
+console.log(null>0);
+console.log(null==0);  //== or >= dono js ke sndr alg tarike se kam krta h 
+
+console.log(null>=0);  //jab compare kiya to js ne null ko 0 me convert kr diya or output true 
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+
+//===   It also check datatype
+console.log("2"===2);
+
+
+
+
+
