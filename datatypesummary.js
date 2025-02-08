108a12132adaac9f32342d66189211b3efbccd90
palakdoremon <palakjn567@gmail.com>
2025-02-08 13:50:29 +0530

    datatypes

---
+++ datatypesummary.js
@@ -0,0 +1,39 @@
+// primitive
+
+//7 categories :  String ,Number,Boolean,Null,undefined,Symbol(used to make a element unique),BigInt
+
+const score=100;
+const scoreValue=100.3
+
+const isLoggedIn =false
+
+const outsideTemp=null
+let useremail; //undefined
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+
+console.log(id==anotherid);
+
+const BigNumber=12364789641212331233n
+
+
+// Reference
+
+//Array,Object,Functions
+
+
+const heros=["srk","varun","salman"];
+
+let myobj={
+    name:"palak",
+    age:19,
+}
+
+const myfunction=function(){
+    console.log("Hello world");
+    
+}
+
+
+
